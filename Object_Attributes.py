e6de17f9f6591b5d4f279da949882c2a97340bf6
osinnikovns <osinnikov_n@mail.ru>
2024-02-15 11:28:48 +0500

    commit

--- Object_Attributes.py
+++ Object_Attributes.py
@@ -1,4 +1,17 @@
+"""Задача:
+1. Создайте новый класс House.
+2. Задайте ему новый атрибут numberOfFloors = 10
+3. В цикле пройдитесь по атрибуту numberOfFloors и распечатайте значение
+"Текущий этаж равен" Полученный код напишите в ответ к домашнему заданию"""
+
+
 class House:
-    numberOfFloors = 10
-    for i in range(0, numberOfFloors):
-        print('Текущий этаж равен:', i + 1)
+    def __init__(self):
+        self.number_of_floors = 10
+
+
+House1 = House()
+print('Всего этажей: ', House1.number_of_floors)
+
+for i in range(0, House1.number_of_floors):
+    print('Текущий этаж равен', i + 1)
