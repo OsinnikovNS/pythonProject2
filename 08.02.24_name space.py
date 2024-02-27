d669323891d8c04e888ea64987d753f216995957
osinnikovns <osinnikov_n@mail.ru>
2024-02-27 15:12:10 +0500

    commit

--- 08.02.24_name space.py
+++ 08.02.24_name space.py
@@ -12,4 +12,4 @@ def test_function():
 # inner_function()  # NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
 
 
-test_function()  # Я в области видимости функции test_function
+test_function()
