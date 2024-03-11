befeb8ff9719e6b8f34f6d55f9bc985e051fa759
osinnikovns <osinnikov_n@mail.ru>
2024-03-11 11:21:49 +0500

    commit

---
+++ 11.03.24_exception handling.py
@@ -0,0 +1,35 @@
+# Освоение обработки исключений в Python для улучшения надёжности и отказоустойчивости программ
+
+def string_to_int(s):  # добавить обработку ValueError
+    try:
+        return int(s)
+    except ValueError:
+        return f"Ошибка: невозможно преобразовать '{s}' в целое число."
+
+
+def read_file(filename):  # добавить обработку FileNotFoundError, IOError
+    try:
+        with open(filename, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return f"Ошибка: файл '{filename}' не найден."
+    except IOError:
+        return f"Ошибка ввода/вывода при работе с файлом '{filename}'."
+
+
+def divide_numbers(a, b):  # добавить обработку ZeroDivisionError, TypeError
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return f"Ошибка: деление на ноль"
+    except TypeError:
+        return f"Ошибка: аргументы должны быть числами"
+
+
+def access_list_element(lst, index):  # добавить обработку IndexError, TypeError
+    try:
+        return lst[index]
+    except IndexError:
+        return f"Ошибка: индекс {index} вне диапазона списка"
+    except TypeError:
+        return f"Ошибка: индекс должен быть целым числом"
