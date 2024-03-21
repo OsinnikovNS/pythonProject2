876e3469f17c8cec850fadff369f238d56365adf
osinnikovns <osinnikov_n@mail.ru>
2024-03-21 16:55:12 +0500

    commit

--- 21.03.24_generators.py
+++ 21.03.24_generators.py
@@ -5,10 +5,27 @@
 
 def all_variants(text):
     for x in range(len(text)):
-        for y in range(x+1, len(text)+1):
+        yield text[x]
+    for x in range(len(text)-1):
+        for y in range(x+2, len(text)):
             yield text[x:y]
+    for x in range(1, len(text)):
+        for y in range(x+2, len(text)+1):
+            yield text[x:y]
+    yield text
 
 
 a = all_variants("abc")
 for i in a:
     print(i)
+
+
+# def all_variants(text):
+#     for x in range(len(text)):
+#         for y in range(x+1, len(text)+1):
+#             yield text[x:y]
+#
+#
+# a = all_variants("abc")
+# for i in a:
+#     print(i)
