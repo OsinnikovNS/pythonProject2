f4e2709be6980e9a81fa48e7dad6300f23c7000b
osinnikovns <osinnikov_n@mail.ru>
2024-03-26 08:58:20 +0500

    commit

---
+++ 31.03.24_Create threads.py
@@ -0,0 +1,24 @@
+import threading
+import time
+
+
+def num():
+    for i in range(1, 11):
+        time.sleep(1)
+        print(i)
+
+
+def my_list():
+    spisok = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    for i in range(10):
+        time.sleep(1.001)
+        print(spisok[i])
+
+
+t1 = threading.Thread(target=num)
+
+t2 = threading.Thread(target=my_list)
+t1.start()
+# t1.join()
+t2.start()
+
