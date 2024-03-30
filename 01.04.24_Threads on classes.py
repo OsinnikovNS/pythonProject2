a15bc11ea54b5ed72ca434ca34f89f2d39ab8e61
osinnikovns <osinnikov_n@mail.ru>
2024-03-30 17:14:19 +0500

    commit

--- 01.04.24_Threads on classes.py
+++ 01.04.24_Threads on classes.py
@@ -10,20 +10,21 @@ class Knight(Thread):
         self.skill = skill
 
     def run(self, attack=100):
+        print(f'{self.name},  на нас напали!')
+        victory = int(attack / self.skill)
         for _ in range(1, attack + 1, self.skill):
             attack = attack - self.skill
             time.sleep(1)
             if attack <= 0:
-                print(f' Рыцарь {self.name}: победа над врагами, умение: {self.skill}')
+                print(f' ********* Рыцарь {self.name} одержал победу спустя {victory} дней! *********')
             else:
-                print(f' Рыцарь {self.name} сражается')
+                print(f' Рыцарь {self.name} сражается, осталось {attack} врагов.')
 
 
 knight1 = Knight("Sir Lancelot", 10)  # Низкий уровень умения
 knight2 = Knight("Sir Galahad", 20)  # Высокий уровень умения
-print('Рыцари начали сражаться.')
-# print('Ждем когда Рыцари победят.')
+
 knight1.start()
-knight1.join()
 knight2.start()
+knight1.join()
 knight2.join()
