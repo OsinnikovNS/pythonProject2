efdec9ebafd4284e972bbf4cc76db96ad5680127
osinnikovns <osinnikov_n@mail.ru>
2024-04-02 11:59:00 +0500

    commit

--- 02.04.24_Thread locks.py
+++ 02.04.24_Thread locks.py
@@ -1,5 +1,5 @@
 import threading
-# import time
+import time
 # Класс BankAccount должен отражать банковский счет с балансом
 # и методами для пополнения и снятия денег.
 
@@ -9,29 +9,35 @@ class BankAccount:
         self.locker = None
         self.lock = threading.Lock()
         self.balance = 1000
+
         print(f'Банковский счет с балансом: {self.balance}')
 
     def deposit_task(self):
         amount = 100
         for _ in range(5):
+            time.sleep(1)
+            self.lock.acquire()
             self.balance += amount
             print(f'Deposited {amount}, new balance is {self.balance}')
+            self.lock.release()
 
     def withdraw_task(self):
         amount = 150
         for _ in range(5):
+            time.sleep(1)
             if self.balance >= amount:
+                self.lock.acquire()
                 self.balance -= amount
                 print(f'Withdraw {amount}, new balance is {self.balance}')
+                self.lock.release()
             else:
                 print("Недостаточно средств на счете")
 
 
 s = BankAccount()
-deposit_thread = threading.Thread(target=s.deposit_task())
-withdraw_thread = threading.Thread(target=s.withdraw_task())
-# deposit_thread.start()
-# withdraw_thread.start()
-# deposit_thread.join()
-# withdraw_thread.join()
-# s.display()
+deposit_thread = threading.Thread(target=s.deposit_task)
+withdraw_thread = threading.Thread(target=s.withdraw_task)
+deposit_thread.start()
+deposit_thread.join()
+withdraw_thread.start()
+withdraw_thread.join()
