9aa324a465855e6aaea69854db1cb007d95634a0
osinnikovns <osinnikov_n@mail.ru>
2024-04-10 21:06:00 +0500

    первоначальная версия

--- 04.04.24_Queues for  threads.py
+++ 04.04.24_Queues for  threads.py
@@ -1,52 +1,62 @@
+import time
 from collections import defaultdict
 import queue
 import threading
 import random
 
-CUSTOMER = (None, 1, 2, 3, 4, 5,)
+
 class Table:
-    pass
+    def __init__(self, number):
+        self.number = number
+        print(f'Создали столик {number}')
+
+
 class Cafe(threading.Thread):
-    def __init__(self, table_for_customer=10, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.customers = []
-        self.table_for_customer = table_for_customer
-        self.tank = queue.Queue(maxsize=3)
+    def __init__(self, *args, **kwargs):
+        # super().__init__(*args, **kwargs)
+        self.customers = ()
+        self.table_for_customer = queue.Queue(maxsize=3)
+        # self.tables = 3
         self.catch = defaultdict(int)
-    # def customer_arrival(self, number):
-    #     customer = Customer(number=number, table=self.table_for_customer, tank=self.tank)
-    #     self.customers.append(customer)
 
     def customer_arrival(self):
-        print(f'Посетитель номер <номер посетителя> прибыл.', flush=True)
-        for customer in self.customers:
-            customer.start()
-        while True:
-            try:
-                customer = self.tank.get(timeout=1)
-                print(f'Столик в кафе принял {customer}', flush=True)
-                self.catch[customer] += 1
-            except queue.Empty:
-                print(f'Столики свободны в течении 1 сек.')
-                if not any(customer.is_alive() for customer in self.customers):
-                    break
+        for self.customers in range(10):
+            self.customers += 1
+            time.sleep(.1)
+            print(f'Посетитель номер {self.customers} прибыл.', flush=True)
+
+    def serve_customer(self, ncustomer):
+        if self.tables is None:
+            print(f'Посетитель номер {ncustomer}, ожидает свободный стол. (помещение в очередь)', flush=True)
+        else:
+            print(f'Посетитель номер {ncustomer}, сел за стол. (начало обслуживания)', flush=True)
+            while True:
+                try:
+                    customer = self.customers.get(timeout=5)
+                    print(f'Столик в кафе принял {customer}', flush=True)
+                    self.catch[customer] += 1
+                except queue.Empty:
+                    print(f'Столики свободны в течении 1 сек.')
+                    if not any(customer.is_alive() for customer in self.customers):
+                        break
         for customer in self.customers:
             customer.join()
         print(f'Кафе закрывается')
+        print(f'Посетитель номер {self.number}, сел за стол. (начало обслуживания)', flush=True)
 
 
-cafe = Cafe(table_for_customer=10)
-for number in CUSTOMER:
-    cafe.customer_arrival()
+table1 = Table(1)
+table2 = Table(2)
+table3 = Table(3)
+tables = [table1, table2, table3]
+
+cafe = Cafe(tables)
+customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
+customer_arrival_thread.start()
 
 cafe.start()
 cafe.join()
 
-
-
-
-# Customer - класс (поток) посетителя. Запускается, если есть свободные столы.
-# class Customer(threading.Thread):
 #     def __init__(self, number, busy, *args, **kwargs):
 #         super().__init__(*args, **kwargs)
 #         self.number = number
@@ -56,6 +66,6 @@ cafe.join()
 #         if customer is None:
 #             print(f'{self.number}, посетителей нет.', flush=True)
 #         else:
-#             print(f'Посетитель номер {self.number}, сел за стол. (начало обслуживания)', flush=True)
+#
 #             if self.busy.full():
-#                 print(f'Посетитель номер {self.number}, ожидает свободный стол. (помещение в очередь)', flush=True)
+#
