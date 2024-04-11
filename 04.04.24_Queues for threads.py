30c0016a337b1f7edeea2574aea095554aa1fd0a
osinnikovns <osinnikov_n@mail.ru>
2024-04-11 17:50:21 +0500

    рабочая версия

--- 04.04.24_Queues for threads.py
+++ 04.04.24_Queues for threads.py
@@ -8,6 +8,7 @@ class Table:
         self.number = number
         self.is_busy = False
 
+
 class Cafe:
     def __init__(self, tables):
         self.queue = queue.Queue()
@@ -17,7 +18,7 @@ class Cafe:
         for i in range(1, 21):
             time.sleep(.01)
             print(f"Посетитель номер {i} прибыл")
-            time.sleep(.1)
+            time.sleep(0.4)
             customer = {"number": i}
             self.queue.put(customer)
 
@@ -27,13 +28,12 @@ class Cafe:
             if not table.is_busy:
                 table.is_busy = True
                 print(f"Посетитель номер {customer['number']} сел за стол {table.number}")
-                time.sleep(.5)
+                time.sleep(2)
                 table.is_busy = False
                 print(f"Посетитель номер {customer['number']} покушал и ушёл")
                 return
-        print(f"Посетитель номер {customer['number']} ожидает свободный стол")
+            print(f"Посетитель номер {customer['number']} ожидает свободный стол")
         self.queue.put(customer)
-
 
 
 def customer_thread(cafe):
@@ -43,6 +43,7 @@ def customer_thread(cafe):
         cafe.queue.task_done()
         if cafe.queue.empty():
             break
+
 
 table1 = Table(1)
 table2 = Table(2)
@@ -59,58 +60,3 @@ for _ in range(3):
     t.start()
 
 customer_arrival_thread.join()
-
-
-#
-# class Table:
-#     def __init__(self, number):
-#         self.number = number
-#         print(f'Создали столик {number}')
-#
-#
-# class Cafe(threading.Thread):
-#     def __init__(self, *args, **kwargs):
-#         # super().__init__(*args, **kwargs)
-#         self.customers = ()
-#         self.table_for_customer = queue.Queue(maxsize=3)
-#         # self.tables = 3
-#         self.catch = defaultdict(int)
-#
-#     def customer_arrival(self):
-#         for self.customers in range(10):
-#             self.customers += 1
-#             time.sleep(.1)
-#             print(f'Посетитель номер {self.customers} прибыл.', flush=True)
-#
-#     def serve_customer(self, ncustomer):
-#         if self.tables is None:
-#             print(f'Посетитель номер {ncustomer}, ожидает свободный стол. (помещение в очередь)', flush=True)
-#         else:
-#             print(f'Посетитель номер {ncustomer}, сел за стол. (начало обслуживания)', flush=True)
-#             while True:
-#                 try:
-#                     customer = self.customers.get(timeout=5)
-#                     print(f'Столик в кафе принял {customer}', flush=True)
-#                     self.catch[customer] += 1
-#                 except queue.Empty:
-#                     print(f'Столики свободны в течении 1 сек.')
-#                     if not any(customer.is_alive() for customer in self.customers):
-#                         break
-#         for customer in self.customers:
-#             customer.join()
-#         print(f'Кафе закрывается')
-#         print(f'Посетитель номер {self.number}, сел за стол. (начало обслуживания)', flush=True)
-#
-#
-# class Customer:
-#     def __init__(self):
-#         pass
-#
-#
-# table1 = Table(1)
-# table2 = Table(2)
-# table3 = Table(3)
-# tables = [table1, table2, table3]
-#
-# cafe = Cafe(tables)
-
