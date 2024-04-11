4597f4ec3482056e5a863b464966be34fd2adb5c
osinnikovns <osinnikov_n@mail.ru>
2024-04-11 16:33:49 +0500

    рабочая версия
    Signed-off-by: osinnikovns <osinnikov_n@mail.ru>

--- 04.04.24_Queues for  threads.py
+++ 04.04.24_Queues for  threads.py
@@ -16,21 +16,20 @@ class Cafe:
 
     def customer_arrival(self):
         for i in range(1, 21):
-            time.sleep(1)
+            time.sleep(.1)
             print(f"Посетитель номер {i} прибыл")
             customer = {"number": i}
             self.queue.put(customer)
 
     def serve_customer(self, customer):
         for table in self.tables:
-            if not table.is_busy:
+            if table.is_busy:
                 table.is_busy = True
-                print(f"Посетитель номер {customer['number']} сел за стол {table.number} (начало обслуживания)")
-                time.sleep(5)
+                print(f"Посетитель номер {customer['number']} сел за стол {table.number}")
+                time.sleep(.5)
                 table.is_busy = False
-                print(f"Посетитель номер {customer['number']} покушал и ушёл (конец обслуживания)")
+                print(f"Посетитель номер {customer['number']} покушал и ушёл")
             return
-            print(f"Посетитель номер {customer['number']} ожидает свободный стол (помещение в очередь)")
         self.queue.put(customer)
 
 
@@ -42,6 +41,7 @@ def customer_thread(cafe):
         if cafe.queue.empty():
             break
 
+
 table1 = Table(1)
 table2 = Table(2)
 table3 = Table(3)
@@ -51,12 +51,12 @@ cafe = Cafe(tables)
 
 customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
 customer_arrival_thread.start()
-
 for _ in range(3):
     t = threading.Thread(target=customer_thread, args=(cafe,))
     t.start()
-
 customer_arrival_thread.join()
+
+
 #
 # class Table:
 #     def __init__(self, number):
@@ -109,8 +109,4 @@ customer_arrival_thread.join()
 # tables = [table1, table2, table3]
 #
 # cafe = Cafe(tables)
-# customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
-# customer_arrival_thread.start()
-#
-# cafe.start()
-# cafe.join()
+
