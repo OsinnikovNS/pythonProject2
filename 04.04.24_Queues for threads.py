944f1cb3cc05293e27d27b8d83dd8e6e44151e89
osinnikovns <osinnikov_n@mail.ru>
2024-04-11 17:30:29 +0500

    рабочая версия

--- 04.04.24_Queues for threads.py
+++ 04.04.24_Queues for threads.py
@@ -8,7 +8,6 @@ class Table:
         self.number = number
         self.is_busy = False
 
-
 class Cafe:
     def __init__(self, tables):
         self.queue = queue.Queue()
@@ -16,21 +15,25 @@ class Cafe:
 
     def customer_arrival(self):
         for i in range(1, 21):
-            time.sleep(.1)
+            time.sleep(.01)
             print(f"Посетитель номер {i} прибыл")
+            time.sleep(.1)
             customer = {"number": i}
             self.queue.put(customer)
 
     def serve_customer(self, customer):
+
         for table in self.tables:
-            if table.is_busy:
+            if not table.is_busy:
                 table.is_busy = True
                 print(f"Посетитель номер {customer['number']} сел за стол {table.number}")
                 time.sleep(.5)
                 table.is_busy = False
                 print(f"Посетитель номер {customer['number']} покушал и ушёл")
-            return
+                return
+        print(f"Посетитель номер {customer['number']} ожидает свободный стол")
         self.queue.put(customer)
+
 
 
 def customer_thread(cafe):
@@ -41,7 +44,6 @@ def customer_thread(cafe):
         if cafe.queue.empty():
             break
 
-
 table1 = Table(1)
 table2 = Table(2)
 table3 = Table(3)
@@ -51,9 +53,11 @@ cafe = Cafe(tables)
 
 customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
 customer_arrival_thread.start()
+
 for _ in range(3):
     t = threading.Thread(target=customer_thread, args=(cafe,))
     t.start()
+
 customer_arrival_thread.join()
 
 
