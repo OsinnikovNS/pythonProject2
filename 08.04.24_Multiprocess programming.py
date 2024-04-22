5dce8921255e4d307ee4d0b621cf41d061924139
osinnikovns <osinnikov_n@mail.ru>
2024-04-22 12:26:38 +0500

    рабочая версия

--- 08.04.24_Multiprocess programming.py
+++ 08.04.24_Multiprocess programming.py
@@ -1,60 +1,58 @@
-requests = [
-    ("product1", "receipt", 100),
-    ("product2", "receipt", 150),
-    ("product1", "shipment", 30),
-    ("product3", "receipt", 200),
-    ("product2", "shipment", 50)
-]
-
-count, product1, product2, product3 = 0, 0, 0, 0
-for i in requests:
-    count += 1
-# print(requests)
-# print(f' elements in the list: {count}')
-
-for i in range(count):
-    d = requests[i]
-    i += 1
-    a, b, c = str(d[0]), str(d[1]), int(d[2])
-    if a == str('product1') and b == str('receipt'):
-        product1 += c
-    if a == str('product1') and b == str('shipment'):
-        product1 -= c
-
-    if a == str('product2') and b == str('receipt'):
-        product2 += c
-    if a == str('product2') and b == str('shipment'):
-        product2 -= c
-
-    if a == str('product3') and b == str('receipt'):
-        product3 += c
-    if a == str('product3') and b == str('shipment'):
-        product3 -= c
-
-    # print(a, b, c)
-print(f'product1: {product1}, product2: {product2}, product3: {product3}')
+"""Движение товаров на складе, запросы на обновление информации о поступлении товаров и отгрузке товаров."""
+import multiprocessing
+import time
 
 
-class WarehouseManager:
-    # класс менеджера склада
-    def __init__(self, data, *args, **kwargs):
-        self.data = {}
-#
-#
-#     def process_request(self):
-#
-#         pass
-#
-#
-# manager = WarehouseManager()
-# requests = [
-#     ("product1", "receipt", 100),
-#     ("product2", "receipt", 150),
-#     ("product1", "shipment", 30),
-#     ("product3", "receipt", 200),
-#     ("product2", "shipment", 50)
-#     ]
-# # Запускаем обработку запросов
-# manager.run(requests)
-# # Выводим обновленные данные о складских запасах
-# print(manager.data)
+class WarehouseManager(multiprocessing.Process):
+    def run(self):
+        requests = [
+            ("product1", "receipt", 100),
+            ("product2", "receipt", 150),
+            ("product1", "shipment", 30),
+            ("product3", "receipt", 200),
+            ("product2", "shipment", 100),
+
+        ]
+        count, product1, product2, product3 = 0, 0, 0, 0
+        for _ in requests:
+            time.sleep(0.1)
+            count += 1
+        print(f'количество запросов: {count}')
+        for i in range(count):
+            d = requests[i]
+            i += 1
+            product, request, data = str(d[0]), str(d[1]), int(d[2])
+# -------------------------- Поступление товара на склад -----------------------------
+            if product == str('product1') and request == str('receipt'):
+                product1 += data
+            if product == str('product2') and request == str('receipt'):
+                product2 += data
+            if product == str('product3') and request == str('receipt'):
+                product3 += data
+# -------------------------- Отгрузка товара со склада -----------------------------
+            if product == str('product1') and request == str('shipment'):
+                if product1 >= data:
+                    product1 -= data
+                else:
+                    print(f'Для отгрузки, товара на складе недостаточно, product1 = {product1}')
+            if product == str('product2') and request == str('shipment'):
+                if product2 >= data:
+                    product2 -= data
+                else:
+                    print(f'Для отгрузки, товара на складе недостаточно, product2 = {product2}')
+            if product == str('product3') and request == str('shipment'):
+                if product == str('product3') and request == str('shipment'):
+                    if product3 >= data:
+                        product3 -= data
+                    else:
+                        print(f'Для отгрузки, товара на складе недостаточно, product3 = {product3}')
+
+        prod = {f'product1: {product1}, product2: {product2}, product3: {product3}'}
+        print(prod)
+
+
+if __name__ == '__main__':
+    manager = WarehouseManager()
+    manager.start()
+    manager.join()
+    print('Все процессы завершены')
