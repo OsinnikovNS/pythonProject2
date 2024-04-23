df3af56f75e92abd46b1a574daadf66fc86b89b1
osinnikovns <osinnikov_n@mail.ru>
2024-04-23 13:48:45 +0500

    рабочая версия

--- 12.04.24_third-party python libraries.py
+++ 12.04.24_third-party python libraries.py
@@ -7,26 +7,31 @@
 # они предоставили по сравнению с базовым функционалом Python.
 # ------------------------------------------------------------------------------
 import requests
+from datetime import datetime as dt
 
 api_key = 'e92c5026eb2a466eb09e1591f5ee035f'
 
-city = input('Введите город (например Moscow): ')
-url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
+# city = input('Введите город (например Moscow): ')
+spisok = ('Yekaterinburg', 'Perm', 'Kazan', 'Berezniki')
+# print(len(str))
+for i in range(len(spisok)):
+    city = spisok[i]
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
+    print(f'*********************  {city}  ***********************')
+    response = requests.get(url)
+    print(f'Погода в {city} на дату: {dt.now().strftime('%d %b %G %H:%M:%S')}')
+    if response.status_code == 200:
+        data = response.json()
+        temp = data['main']['temp']
+        pressure = data['main']['pressure']
+        humidity = data['main']['humidity']
+        visibility = data['visibility']
+        desc = data['weather'][0]['description']
 
-response = requests.get(url)
-
-if response.status_code == 200:
-    data = response.json()
-    temp = data['main']['temp']
-    pressure = data['main']['pressure']
-    humidity = data['main']['humidity']
-    visibility = data['visibility']
-    desc = data['weather'][0]['description']
-
-    print(f'Температура: {temp} C')
-    print(f'Давление: {pressure} мм')
-    print(f'Влажность: {humidity} %')
-    print(f'Видимость: {visibility} м')
-    print(f'Описание: {desc}')
-else:
-    print('Ошибка при получении данных о погоде')
+        print(f'Температура: {temp} C')
+        print(f'Давление: {pressure} мм')
+        print(f'Влажность: {humidity} %')
+        print(f'Видимость: {visibility} м')
+        print(f'Описание: {desc}')
+    else:
+        print('Ошибка при получении данных о погоде')
