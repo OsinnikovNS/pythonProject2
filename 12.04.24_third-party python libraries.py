4eecdb68f3fd0ba49a2ebb8300f9f237d04de841
osinnikovns <osinnikov_n@mail.ru>
2024-04-23 18:01:36 +0500

    добавлен список городов, добавлена дата и время в отчет.

--- 12.04.24_third-party python libraries.py
+++ 12.04.24_third-party python libraries.py
@@ -6,32 +6,43 @@
 # Поясните, как выбранные библиотеки помогли в решении поставленных задач и какие преимущества
 # они предоставили по сравнению с базовым функционалом Python.
 # ------------------------------------------------------------------------------
+from api_key import API_KEY
 import requests
 from datetime import datetime as dt
 
-api_key = 'e92c5026eb2a466eb09e1591f5ee035f'
 # для возможности ввода наименования города с клавиатуры
 # city = input('Введите город (например Moscow): ')
-spisok = ('Yekaterinburg', 'Perm', 'Kazan', 'Berezniki')
-# print(len(str))
+# https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} - вывод по широте и долготе.
+spisok = (
+            'Yekaterinburg',
+            '     Perm    ',
+            '    Kazan    ',
+            '  Berezniki  '
+        )
 for i in range(len(spisok)):
     city = spisok[i]
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
-    print(f'*********************  {city}  ***********************')
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}'
+    print(f'********************* {city} ***********************')
     response = requests.get(url)
     print(f'Погода в {city} на дату: {dt.now().strftime('%d %b %G %H:%M:%S')}')
     if response.status_code == 200:
         data = response.json()
         temp = data['main']['temp']
-        pressure = data['main']['pressure']
+        wind_speed = data['wind']['speed']
         humidity = data['main']['humidity']
+        clouds = data['clouds']['all']
+        # precipitation = data['main']['precipitation']
         visibility = data['visibility']
+        pressure = data['main']['pressure']
         desc = data['weather'][0]['description']
 
-        print(f'Температура: {temp} C')
-        print(f'Давление: {pressure} мм')
+        print(f'Температура: {temp} °C')
+        print(f'Скорость ветра: {wind_speed} м/с')
         print(f'Влажность: {humidity} %')
+        print(f'Облачность: {clouds} %')
+        # print(f'Осадки: {precipitation} мм/ч')
         print(f'Видимость: {visibility} м')
-        print(f'Описание: {desc}')
+        print(f'Давление: {pressure} мм')
+        # print(f'Описание: {desc}')
     else:
         print('Ошибка при получении данных о погоде')
