a15798a83bce86831a13bcda302bfbdfdce7d890
osinnikovns <osinnikov_n@mail.ru>
2024-06-04 10:17:44 +0500

    рабочая версия

--- Loop for_ List items.py
+++ Loop for_ List items.py
@@ -9,12 +9,15 @@ for i in range(0, 11, 2):
     print(cars_count, end=' ')
 # end=' '- вывод данных для печати в одну строку через пробел
 # для печати с новой строки используем команду print(end='\n') - (перевод на новую строку)
-print(end='\n'), print(end='\n'), print('Конец подсчета.')
+print('Конец подсчета.')
+print(end='\n'), print(end='\n')
+print('Конец подсчета.')
 
-# второе решение задачи:
+print(end='\n'), print(end='\n')
+print('Второе решение задачи:')
 cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 cars_count = 0
 for car in cars:
-    print('Я езжу на автомобиле марки:', car)
     cars_count += 10
+    print('Я езжу на автомобиле марки:', car)
     print(cars_count)
