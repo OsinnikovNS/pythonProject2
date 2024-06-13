2f8ff183c0d7b8f970114536dedb028a900a4553
osinnikovns <osinnikov_n@mail.ru>
2024-06-13 15:41:16 +0500

    рабочая версия

--- Regular expressions/24.05.24_regular_expressions.py
+++ Regular expressions/24.05.24_regular_expressions.py
@@ -1,15 +1,19 @@
 import re
 
 sample_html = ("<img src='https://example.com/image1.jpg'> "
-               "<img src='http://example.com/image2.png'> <im "
-               "src='https://example.com/image3.gif'>")
+               "<img src='http://example.com/image2.png'> "
+               "<img src='https://example.com/image3.gif'>")
 
 
 def extract_image_links(text):
-    pattern = r'([^\']+(?:png|jpg|gif))'
+    pattern = r'([^\']+(?:png|jpg|jpeg|gif))'
     text = re.finditer(pattern, sample_html)
-    for matched in text:
-        print(f'{matched.group()}')
+
+    if text:
+        for matched in text:
+            print(f'{matched.group()}')
+    else:
+        print("Нет ссылок с картинками в HTML тексте.")
 
 
 image_links = extract_image_links(sample_html)
